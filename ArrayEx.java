2ed7782ef137d5e8cf1e71e0bb4c8c59d1582ec2
Janhavi Mali <167394492+janhaviM15@users.noreply.github.com>
2025-08-26 11:14:39 +0530

    Add files via upload

---
+++ ArrayEx.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class ArrayEx {
+
+	public static void main(String[] args) 
+	{
+		int[]j = {78,88,54};
+		
+		for(int i=0;i<j.length;i++ )
+		{
+			System.out.println(j[i]);
+		}
+	}
+
+}
